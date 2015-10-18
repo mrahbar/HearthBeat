ba6875bc4524e8c29b14f0ed70f18cc58b3b35bc
Mahmoud Reza Rahbar Azad <mrahbar.azad@gmail.com>
2015-10-18 22:34:30 +0200

    fixed testrunner write first test for showing fab

---
+++ app/src/androidTest/java/com/smartsoftware/android/hearthbeat/MainUITest.java
@@ -0,0 +1,36 @@
+package com.smartsoftware.android.hearthbeat;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.smartsoftware.android.hearthbeat.main.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static com.smartsoftware.android.hearthbeat.EspressoHelper.checkViewByIdVisible;
+
+/*
+https://github.com/googlesamples/android-testing-templates
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainUITest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityRule = new ActivityTestRule<>(MainActivity.class,
+            true,     // initialTouchMode
+            false);   // launchActivity. False so we can customize the intent per test method
+
+    private void launchActivity() {
+        activityRule.launchActivity(new Intent());
+    }
+
+    @Test
+    public void FabVisibleOnDecksPage() {
+        launchActivity();
+
+        checkViewByIdVisible(R.id.main_deck_add);
+    }
+}
--- app/src/main/java/com/smartsoftware/android/hearthbeat/main/MainActivity.java
+++ app/src/main/java/com/smartsoftware/android/hearthbeat/main/MainActivity.java
@@ -4,10 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.smartsoftware.android.hearthbeat.R;
-import com.smartsoftware.android.hearthbeat.model.Card;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 import io.realm.Realm;
 
@@ -22,28 +18,11 @@ public class MainActivity extends AppCompatActivity {
 
         // Open the default realm for the UI thread.
         realm = Realm.getInstance(this);
-
-        findViewById(R.id.test_btn).setOnClickListener(v -> {
-            final Card card= realm.where(Card.class).equalTo("id", "CS1_129").findFirst();
-            card.getId();
-        });
-
-        findViewById(R.id.go_btn).setOnClickListener(v -> loadJsonFromStream());
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         realm.close();
-    }
-
-    private void loadJsonFromStream(){
-        realm.executeTransaction(realm -> {
-            try(InputStream stream = getAssets().open("deDE_mod.json")) {
-                realm.createAllFromJson(Card.class, stream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
     }
 }
